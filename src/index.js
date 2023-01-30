bccc9bdb63d9368f54c03d0771994aebc4d335de
katalimbu <katalina.esteban@gmail.com>
2023-01-30 10:27:16 -0300

    card validation completo

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 import validator from './validator.js';
 
-console.log(validator);
+
+function validate() { 
+  const inputValue = document.getElementById("cardnumber").value
+          
+  if (inputValue === '') {
+    alert('campo requerido');
+    return;
+  } 
+
+  const valid = validator.isValid(inputValue); 
+  // valid = true/false
+  if(valid === true){
+    alert('tarjeta valida')
+  } 
+  else{
+    alert('tarjeta invalida')
+  }  
+}
+
+const btn = document.querySelector('#validar')
+
+btn.addEventListener('click', () => {
+  validate()
+})
+
+const cajatexto = document.getElementById('cardnumber');
+cajatexto.addEventListener('change', updateValue )
+
+// const maskify = document.getElementById('valores');
+
+// input.addEventListener('input', updateValue);
+
+// function updateValue(e) {
+//   log.textContent = e.srcElement.value;
+// }
+
+// const input = document.querySelector('input');
+const log = document.getElementById('log');
+
+// input.addEventListener('change', updateValue);
+
+function updateValue(e) {
+  log.textContent =validator.maskify(e.target.value);
+}
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,69 @@
 const validator = {
-  // ...
-};
+  maskify(creditCard) {
+    if (creditCard.length < 5) return creditCard;
+    const last4Characters = creditCard.substr(-4);
+    const maskingCharacters = creditCard.substr(0, creditCard.length - 4).replace(/\d/g, '#').replace(/[a-z]/g, '#');
+    return `${maskingCharacters}${last4Characters}`;
+  },
+  //los metodos dentro de un objeto se separan en comas 
+  isValid(creditCardNumber) {
+        
+    // ej: creditCardNumber = '6666'
+    const array  = creditCardNumber.split('')
+    // array =['6','6','7','8']
+    const card = array;   
+    // card = ['6','6','7','8']
+    const cambio = card.reverse();
+    // cambio = ['8','7','6','6']
+    const multiready = []
 
+    for (let i = 0; i < cambio.length; i++){
+      if (i % 2 === 1) { 
+        const multiplicacion = cambio[i] * 2
+        multiready.push(multiplicacion)
+      }
+      else {
+        multiready.push(cambio[i])
+      }
+    }
+
+    // multiready = ['8',14,'6',12]  
+    const str = multiready.toString().split(',');
+    // str = ['8','14','6','12']  
+    let total = 0;
+
+    for (let i = 0; i < str.length; i++){
+     
+    
+      if (parseInt(str[i]) >= 10) { 
+        const numero = str[i].split('');
+        const arrayEntero = numero.map( x => parseInt(x));
+        const suma = arrayEntero[0] + arrayEntero[1];
+        // suma = 5
+
+        
+        total = total + suma
+        
+
+      }
+      else {
+       
+
+        total = total + parseInt(str[i])
+        
+      } 
+
+    
+    }
+
+    if ((total % 10) === 0) {
+      return true
+    }
+    else{
+      return false
+    }
+    // is creditCardNumber valid?
+
+  }
+}
 export default validator;
